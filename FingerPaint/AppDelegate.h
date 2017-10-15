05ad42c4d1ab92fc4b07955d2206881bd537719c
Andrew Shopland <hi@ashp.land>
2017-10-15 11:52:24 -0700

    Initial Commit

---
+++ FingerPaint/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FingerPaint
+//
+//  Created by Andrew on 2017-10-15.
+//  Copyright © 2017 Andrew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FingerPaint/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FingerPaint
+//
+//  Created by Andrew on 2017-10-15.
+//  Copyright © 2017 Andrew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
